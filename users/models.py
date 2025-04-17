1af1b4058c98a7acec069cec858a633ca02c06ff
kubrasade <kubra.sade2@icloud.com>
2025-04-17 11:34:24 +0300

    Add: UserManager

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from core.models import BaseModel
+from core.enums import UserType, Gender
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email).strip()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('user_type', UserType.ADMIN)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if not extra_fields.get('is_superuser'):
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
