f439b2c2323ae9d876b011b20ca953ae57ad638d
kubrasade <kubra.sade2@icloud.com>
2025-04-23 10:40:57 +0300

    Add: MatchModel

--- match/models.py
+++ match/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from core.models import BaseModel
-from users.models import ClientProfile, Specialization
+from users.models import ClientProfile, Specialization, DietitianProfile
 from django.contrib.auth import get_user_model
+from core.enums import MatchingStatus
 
 User = get_user_model()
 
@@ -15,3 +16,21 @@ class SpecializationChoice(BaseModel):
 
     def __str__(self):
         return f"{self.client.user.get_full_name()} - {self.specialization.name}"
+    
+class Matching(BaseModel):
+    client = models.ForeignKey(ClientProfile, on_delete=models.CASCADE, related_name='matchings')
+    dietitian = models.ForeignKey(DietitianProfile, on_delete=models.CASCADE, related_name='matchings')
+    specialization = models.ForeignKey(Specialization, on_delete=models.CASCADE, related_name='matchings')
+    status = models.PositiveSmallIntegerField(
+        choices=MatchingStatus.choices,
+        default=MatchingStatus.PENDING
+    )
+    matched_at = models.DateTimeField(null=True, blank=True)
+    ended_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        unique_together = ('client', 'dietitian', 'specialization')
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.client.user.get_full_name()} - {self.dietitian.user.get_full_name()} ({self.specialization.name})"
