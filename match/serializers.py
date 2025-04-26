4593b861ccafdd47c05b8609d88aa37cdaef2346
kubrasade <kubra.sade2@icloud.com>
2025-04-26 08:01:28 +0300

    Add: SimpleUserSerializer

---
+++ match/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import MatchModel, Review, SpecializationChoice
+from users.serializers import  SpecializationSerializer
+from users.models import DietitianProfile, ClientProfile, Specialization
+from django.db.models import Avg
+from core.enums import MatchingStatus, ReviewStatus
+from .services import MatchingService, ReviewService
+from django.contrib.auth import get_user_model
+
+User= get_user_model()
+
+class SimpleUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name')
+
