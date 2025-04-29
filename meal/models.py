303d35da540480ad06080a1378bc0eb7370383e5
kubrasade <kubra.sade2@icloud.com>
2025-04-29 08:07:43 +0300

    Add: MealCategory model

--- meal/models.py
+++ meal/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import MinValueValidator, MaxValueValidator
+from core.models import BaseModel
 
 class DietaryTag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class MealCategory(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='meal_categories/', null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Meal Categories'
 
     def __str__(self):
         return self.name
