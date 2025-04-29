a8541a529e349296863c9c46b7bd0790124c8937
kubrasade <kubra.sade2@icloud.com>
2025-04-29 08:09:13 +0300

    Add: Ingredient model

--- meal/models.py
+++ meal/models.py
@@ -22,3 +22,17 @@ class MealCategory(BaseModel):
 
     def __str__(self):
         return self.name
+
+class Ingredient(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    calories_per_100g = models.FloatField(validators=[MinValueValidator(0)])
+    protein_per_100g = models.FloatField(validators=[MinValueValidator(0)])
+    carbs_per_100g = models.FloatField(validators=[MinValueValidator(0)])
+    fat_per_100g = models.FloatField(validators=[MinValueValidator(0)])
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
