f21fd4baa43ec2e7fdd6c9127ec292c2c8c99039
kubrasade <kubra.sade2@icloud.com>
2025-04-30 10:45:19 +0300

    Add: MuscleGroup model

--- traning/models.py
+++ traning/models.py
@@ -45,6 +45,15 @@ class Exercise(BaseModel):
     def __str__(self):
         return self.name
 
+class MuscleGroup(BaseModel):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='muscle_groups/', null=True, blank=True)
 
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 
