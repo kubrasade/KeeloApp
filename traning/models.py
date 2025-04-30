d0626680935f9cb398e046c6013551de4e41bd14
kubrasade <kubra.sade2@icloud.com>
2025-04-30 10:47:04 +0300

    Add: Workout model

--- traning/models.py
+++ traning/models.py
@@ -67,6 +67,26 @@ class Equipment(BaseModel):
     def __str__(self):
         return self.name
 
+class Workout(BaseModel):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    difficulty = models.PositiveSmallIntegerField(choices=Difficulty_Type.choices)
+    duration = models.PositiveIntegerField(help_text="Duration in minutes")
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='created_workouts')
+    approved_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='approved_workouts'
+    )
+    target_muscle_groups = models.ManyToManyField(MuscleGroup, related_name='workouts')
+    equipment_needed = models.ManyToManyField(Equipment, related_name='workouts', blank=True)
 
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 
