f011ee7938252819be0b46345afb493cffb921ab
kubrasade <kubra.sade2@icloud.com>
2025-05-01 08:33:59 +0300

    Add: MuscleGroupSerializer

---
+++ traning/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import (
+    ExerciseCategory,
+    Exercise,
+    MuscleGroup,
+    Equipment,
+    Workout,
+    WorkoutExercise,
+    WorkoutPlan,
+    WorkoutPlanDay,
+    Progress,
+    PerformanceMetric
+)
+from django.db.models import Avg
+
+class MuscleGroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MuscleGroup
+        fields = ['id', 'name', 'description', 'image', 'created_at', 'updated_at']
+        read_only_fields = ['created_at', 'updated_at']
