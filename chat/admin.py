4f98ddcca29c659d3b57f08f854e4045e3f84bd5
kubrasade <kubra.sade2@icloud.com>
2025-05-03 09:29:02 +0300

    Add: admin panel customization

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ChatRoom, Message, MessageRead
 
-# Register your models here.
+@admin.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'dietitian', 'created_at')
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'chat_room', 'sender', 'content', 'created_at')
+
+@admin.register(MessageRead)
+class MessageReadAdmin(admin.ModelAdmin):
+    list_display = ('id', 'message', 'user', 'read_at')
