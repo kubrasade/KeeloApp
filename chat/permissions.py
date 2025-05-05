57437d1426adb7dcfaa8aa8b44b4c497e3116786
kubrasade <kubra.sade2@icloud.com>
2025-05-05 08:23:51 +0300

    Add: IsClientOrDietitian permission

--- chat/permissions.py
+++ chat/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 from .models import ChatRoom, Message
-
+from core.enums import UserType  
 
 class IsChatRoomParticipant(permissions.BasePermission):
     def has_object_permission(self, request, view, obj: ChatRoom):
@@ -18,3 +18,12 @@ class IsMessageSender(permissions.BasePermission):
                 obj.chat_room.dietitian == request.user
             )
         return obj.sender == request.user
+    
+
+
+class IsClientOrDietitian(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            hasattr(request.user, 'user_type') and
+            request.user.user_type in [UserType.CLIENT, UserType.DIETITIAN]
+        )
